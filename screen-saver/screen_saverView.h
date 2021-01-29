e15ff286c7ac5e97b6e2232ebc0f3679201c007d
lib <dreak603@gmail.com>
2021-01-29 18:15:58 +0800

    chore: create xcode

---
+++ screen-saver/screen_saverView.h
@@ -0,0 +1,12 @@
+//
+//  screen_saverView.h
+//  screen-saver
+//
+//  Created by 李韻晟 on 2021/1/29.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface screen_saverView : ScreenSaverView
+
+@end
---
+++ screen-saver/screen_saverView.m
@@ -0,0 +1,51 @@
+//
+//  screen_saverView.m
+//  screen-saver
+//
+//  Created by 李韻晟 on 2021/1/29.
+//
+
+#import "screen_saverView.h"
+
+@implementation screen_saverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
