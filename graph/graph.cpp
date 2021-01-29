c2cbc409a39640e5a963e1ec34715ac1616df2e4
李韻晟 <dreak603@gmail.com>
2021-01-29 10:55:36 +0800

    chore: rename Class to Uppercase

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -1,87 +1,87 @@
 #include "graph.hpp"
 
 namespace cjh {
-  vertex::vertex(double x, double y) {
+  Vertex::Vertex(double x, double y) {
     coords[0] = x;
     coords[1] = y;
   };
 
   // accessor operartor
-  double& vertex::operator[](int idx) {
+  double& Vertex::operator[](int idx) {
     return coords[idx];
   };
 
-  const double& vertex::operator[](int idx) const {
+  const double& Vertex::operator[](int idx) const {
     return coords[idx];
   };
 
   // get data methods
-  struct vertex::data_type& vertex::getData() {
+  struct Vertex::data_type& Vertex::getData() {
     return data;
   };
-  const struct vertex::data_type& vertex::getData() const {
+  const struct Vertex::data_type& Vertex::getData() const {
     return data;
   };
 
   // struct data
 
   // modify graph state
-  void graph::addVertex(const vertex_type& v) {
+  void Graph::addVertex(const vertex_type& v) {
     vertices.push_back(v);
     connectivity.push_back(list<uint32_t>());
   };
 
-  void graph::addVertex(double x, double y){
+  void Graph::addVertex(double x, double y){
     vertex_type vt;
     vt[0] = x; vt[1] = y;
     this -> addVertex(vt);
   };
 
-  void graph::addEdge(uint32_t from_vertex, uint32_t to_vertex) {
+  void Graph::addEdge(uint32_t from_vertex, uint32_t to_vertex) {
     connectivity[from_vertex].push_back(to_vertex);
   };
 
   // get vertex
-  typename graph::vertex_type& graph::getVertex(uint32_t id) {
+  typename Graph::vertex_type& Graph::getVertex(uint32_t id) {
     return vertices[id];
   };
 
-  const typename  graph::vertex_type& graph::getVertex(uint32_t id) const {
+  const typename  Graph::vertex_type& Graph::getVertex(uint32_t id) const {
     return vertices[id];
   };
 
   // get container with vertics
-  vector<typename graph::vertex_type>& graph::getVertices() {
+  vector<typename Graph::vertex_type>& Graph::getVertices() {
     return vertices;
   };
 
-  const vector<typename graph::vertex_type>& graph::getVertices() const {
+  const vector<typename Graph::vertex_type>& Graph::getVertices() const {
     return vertices;
   };
 
   // get iterators to move thorough connect info
-  typename graph::list_type::iterator graph::getConnectivityStartIterator(uint32_t from_vertex_id) {
+  typename Graph::list_type::iterator Graph::getConnectivityStartIterator(uint32_t from_vertex_id) {
     return connectivity[from_vertex_id].begin();
   };
 
-  typename graph::list_type::const_iterator graph::getConnectivityStartIterator(uint32_t from_vertex_id) const {
+  typename Graph::list_type::const_iterator Graph::getConnectivityStartIterator(uint32_t from_vertex_id) const {
     return connectivity[from_vertex_id].begin();
   };
 
-  typename graph::list_type::iterator graph::getConnectivityEndIterator(uint32_t to_vertex_id) {
+  typename Graph::list_type::iterator Graph::getConnectivityEndIterator(uint32_t to_vertex_id) {
     return connectivity[to_vertex_id].end();
   };
 
-  typename graph::list_type::const_iterator graph::getConnectivityEndIterator(uint32_t to_vertex_id) const {
+  typename Graph::list_type::const_iterator Graph::getConnectivityEndIterator(uint32_t to_vertex_id) const {
     return connectivity[to_vertex_id].end();
   };
 
   // clear the graph
-  void graph::clear() {
+  void Graph::clear() {
     vertices.clear();
     connectivity.clear();
   };
 
-  vector<typename graph::vertex_type> vertices;
-  vector<typename graph::list_type> connectivity;
+  vector<typename Graph::vertex_type> vertices;
+  vector<typename Graph::list_type> connectivity;
 }
---
+++ simulator/simulator.cpp
@@ -0,0 +1,31 @@
+#include "simulator.hpp"
+
+namespace cjh {
+
+  // getter/setter for internal state
+    void Simulator::setTime(double t) {
+
+    }
+
+    double Simulator::getTime() const {
+
+    }
+
+    void Simulator::setTimeStep(double timestep) {
+
+    }
+
+    double Simulator::getTimeStep() const {
+
+    }
+
+    void Simulator::setCenter(double x, double y) {
+
+    }
+
+
+    // update graph dynamic
+    void Simulator::updateGraphDynamics(Graph& G) {
+
+    }
+}
